2a6e259b2b0c306cac4f46e5446f606ea4306adb
Anmol Mittal <mittalanmol22@gmail.com>
2017-06-01 01:00:35 +0530

    Just one last wakelock toggle :P

--- drivers/base/power/wakeup.c
+++ drivers/base/power/wakeup.c
@@ -35,6 +35,8 @@ static bool enable_si_ws = true;
 module_param(enable_si_ws, bool, 0644);
 static bool enable_msm_hsic_ws = true;
 module_param(enable_msm_hsic_ws, bool, 0644);
+static bool enable_smb135x_wake_ws = true;
+module_param(enable_smb135x_wake_ws, bool, 0644);
 
 /*
  * If set, the suspend/hibernate code will abort transitions to a sleep state
@@ -507,7 +509,8 @@ static void wakeup_source_activate(struct wakeup_source *ws)
 		(!enable_ipa_ws &&
 			!strcmp(ws->name, "IPA_WS")) ||
                 (!enable_si_ws && !strcmp(ws->name, "sensor_ind")) ||
-                (!enable_msm_hsic_ws && !strcmp(ws->name, "msm_hsic_host"))) {
+                (!enable_msm_hsic_ws && !strcmp(ws->name, "msm_hsic_host")) ||
+                (!enable_smb135x_wake_ws && !strcmp(ws->name, "smb135x_wake_ws"))) {
 		/*
 		 * let's try and deactivate this wakeup source since the user
 		 * clearly doesn't want it. The user is responsible for any
